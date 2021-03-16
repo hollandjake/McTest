879e4ff448f62148ba517e6a0d1af249b164f32c
Jake Holland <hollandjake@hotmail.co.uk>
2021-03-16 19:05:31 +0000

    Cleaned up the TruthTable

--- src/main/java/com3529/TruthTable.java
+++ src/main/java/com3529/TruthTable.java
@@ -2,91 +2,67 @@ package com3529;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.github.javaparser.ast.expr.BinaryExpr;
-import com.github.javaparser.ast.expr.EnclosedExpr;
-import com.github.javaparser.ast.expr.SimpleName;
 
-import com.github.javaparser.ast.type.Type;
-
-import lombok.Data;
 import org.mariuszgromada.math.mxparser.Expression;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import lombok.Data;
 
 @Data
 public class TruthTable
 {
+    private static final Pattern GET_OPERATORS = Pattern.compile("(\\|\\|)|(&&)");
+
     private final List<ConditionPredicate> conditionPredicates;
 
-    public static TruthTable from(BinaryExpr expression) throws ScriptException {
-        // a && b
-        // a && b || c
-        // Operators = [OR, AND]
-        // Conditions = [FALSE, FALSE, FALSE]
-        // new conditions = [false, false]
-        // new operators = [AND]
-        // new condtions = [false]
-        // new operators = []
-        // 1. Loop Operators for AND index i
-        // 2. Conditions[i] Operator[i] Conditions[i+1]
-        // = FALSE && FALSE
-        // Predicate = FALSE
+    public static TruthTable from(BinaryExpr expression)
+    {
 
-        final String regex = "(\\|\\|)|(&&)";
-        final String string = expression.toString();
+        String expressionString = expression.toString();
 
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
-        final Matcher matcher = pattern.matcher(string);
+        final Matcher matcher = TruthTable.GET_OPERATORS.matcher(expressionString);
 
-        List<BinaryExpr.Operator> operators = new ArrayList<>();
-        while (matcher.find()) {
-            if (matcher.group(1) != null) {
-                operators.add(BinaryExpr.Operator.OR);
-            }  else {
-                operators.add(BinaryExpr.Operator.AND);
-            }
+        List<String> operators = new ArrayList<>();
+        while (matcher.find())
+        {
+            operators.add(matcher.group(0));
         }
 
         int n = operators.size() + 1;
-        double m = Math.pow(2,n);
+
         List<ConditionPredicate> conditionPredicates = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
+
+        String binaryFormatPattern = "%" + n + "s";
+
+        //Generate all permutations of conditions and evaluate the expressions
+        for (int i = 0; i < 1 << n; i++)
+        {
+            StringBuilder testString = new StringBuilder();
             List<Boolean> conditions = new ArrayList<>();
-            String binary = String.format("%"+n+"s", Integer.toBinaryString(i)).replace(' ', '0');
-            for (char ch: binary.toCharArray()) {
-                if (ch == '0') {
-                    conditions.add(false);
-                } else {
-                    conditions.add(true);
+            char[] binaryCharSequence = String.format(binaryFormatPattern, Integer.toBinaryString(i)).toCharArray();
+            int j = 0;
+
+            for (char c : binaryCharSequence)
+            {
+                boolean isTruthy = c == '1';
+                conditions.add(isTruthy);
+                testString.append(isTruthy ? '1' : '0');
+
+                if (j < n - 1)
+                {
+                    testString.append(operators.get(j));
+                    j++;
                 }
             }
 
-            String runthis = "";
-            for (int z = 0; z < conditions.size(); z++) {
-                runthis += conditions.get(z) ? "1" : "0";
-                if (z != conditions.size() - 1) {
-                    runthis += operators.get(z).asString();
-                }
-            }
-
-            Expression e = new Expression(runthis);
-            double v = e.calculate();
-            boolean predicate = v == 1;
+            boolean predicate = new Expression(testString.toString()).calculate() == 1;
 
             conditionPredicates.add(new ConditionPredicate(conditions, predicate));
         }
 
-        //MCDC
-        //
-
         return new TruthTable(conditionPredicates);
     }
-
-
 }
