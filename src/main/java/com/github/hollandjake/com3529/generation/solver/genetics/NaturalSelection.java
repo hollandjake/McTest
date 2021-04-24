69d73329f7021a4524184f75f305b4c870f10954
Jake Holland <hollandjake@hotmail.co.uk>
2021-04-24 11:27:31 +0100

    Added JavaDocs for the genetics package

--- src/main/java/com/github/hollandjake/com3529/generation/solver/genetics/NaturalSelection.java
+++ src/main/java/com/github/hollandjake/com3529/generation/solver/genetics/NaturalSelection.java
@@ -12,14 +12,28 @@ import lombok.Getter;
 import lombok.experimental.Accessors;
 import lombok.experimental.UtilityClass;
 
+/**
+ * Class responsible for handling the selection of the individuals who should progress to the next generation
+ */
 @UtilityClass
 public class NaturalSelection
 {
+    /**
+     * How many individuals to bring into the next generation from the previous generation
+     */
     @Accessors(fluent = true)
     @Getter(value = AccessLevel.PACKAGE,
             lazy = true)
     private static final int TOP_N = ConfigFactory.load().getInt("Genetics.TopN");
 
+    /**
+     * Return the {@link #TOP_N} individuals sorted by ascending fitness
+     *
+     * i.e. Select the {@link #TOP_N} fittest individuals
+     *
+     * @param population The current population
+     * @return A subset of the population who are the fittest {@link #TOP_N} individuals
+     */
     public static List<MethodTestSuite> overPopulation(List<MethodTestSuite> population)
     {
         return population
